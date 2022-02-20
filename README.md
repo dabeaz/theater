825b77bdba0e5a1d1f17209a3096e6ee1474ffbc
David Beazley <dave@dabeaz.com>
2022-02-20 15:03:06 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # theater
-Code for "The Problem with the Problem" talk, February 22, 2022. 
+
+Code for "The Problem with the Problem" talk, given by
+David Beazley (@dabeaz) at PyCon Sri Lanka, February 22, 2022.
+
+
---
+++ theater.py
@@ -0,0 +1,30 @@
+# https://github.com/dabeaz/theater
+#
+# The owner of a monopolistic movie theater in a small town has
+# complete freedom in setting ticket prices.  The more he charges, the
+# fewer people can afford tickets.  The less he charges, the more it
+# costs to run a show because attendance goes up.  In a recent
+# experiment the owner determined a relationship between the price of
+# a ticket and average attendance.
+#
+# At a price of $5.00/ticket, 120 people attend a performance.  For
+# each 10-cent change in the ticket price, the average attendance
+# changes by 15 people.  That is, if the owner charges $5.10, some 105
+# people attend on the average; if the price goes down to $4.90,
+# average attendance increases to 135.
+#
+# Unfortunately, the increased attendance also comes at an increased
+# cost.  Every performance comes at a fixed cost of $180 to the owner
+# plus a variable cost of $0.04 per attendee.
+#
+# The owner would like to know the exact relationship between profit
+# and ticket price in order to maximize the profit.
+#
+# Write a program to figure out the best ticket price (to the nearest
+# 10 cents) that maximizes profit.
+#
+# Credit: This problem comes from "How to Design Programs", 2nd Ed.
+# by Felleisen, Findler, Flatt, and Krishnamurthi.  pg. 60.
+
+
+
